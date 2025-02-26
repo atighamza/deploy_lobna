6df3ebe3ddf62f3be3120ba5188786652cf2a9d6
hamza atig <hamzaatig2000@gmail.com>
2025-02-26 14:43:05 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+//connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
